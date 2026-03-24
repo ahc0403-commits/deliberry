f9a926362a18ec4c3e09bf75c0b061968611c376
안효창 <andremacmini@Mac.lan>
2026-03-24 07:23:33 +0700

    Move Zalo verification meta tag to app head

---
+++ public-website/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="zalo-platform-site-verification"
+        content="PzwL3RVB91jirhL5xSPYRHv5q7VximKfCpav"
+      />
+    </>
+  );
+}
