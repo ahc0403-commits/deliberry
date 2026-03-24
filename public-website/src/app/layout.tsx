52eb51a5e745fa6e23facdf57b5503a415de93d9
안효창 <andremacmini@Mac.lan>
2026-03-24 07:13:41 +0700

    Add Zalo domain verification meta tag

--- public-website/src/app/layout.tsx
+++ public-website/src/app/layout.tsx
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="zalo-platform-site-verification"
+          content="PzwL3RVB91jirhL5xSPYRHv5q7VximKfCpav"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
